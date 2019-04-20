89897765dc94976efcba8612a3cfd924a9ee7b86
AlexandrMartyukhin <megacraft@gmail.com>
2019-04-20 21:49:36 +0300

    Add pic, add ru-locale, add style, + resources

--- app/src/main/java/ru/minilan/helloworldlab/SecondActivity.java
+++ app/src/main/java/ru/minilan/helloworldlab/SecondActivity.java
@@ -1,10 +1,11 @@
 package ru.minilan.helloworldlab;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -20,21 +21,28 @@ public class SecondActivity extends AppCompatActivity {
         TextView textViewTomorrowDay = findViewById(R.id.textViewTomorrowDay);
         TextView textViewTomorrowNight = findViewById(R.id.textViewTomorrowNight);
 
-        String header = getResources().getText(R.string.app_header_in_town_X)+" "+
-                getIntent().getExtras().getString(MainActivity.TOWN);
-        textviewheadertown.setText(header);
+        String town;
+        if (((town = getIntent().getExtras().getString(MainActivity.TOWN)) == null) || (town.equals(""))) {
+            town = getResources().getString(R.string.def_town);
+            Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.show_def_town),
+                    Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+        }
 
+        String header = getResources().getText(R.string.app_header_in_town_X) + " " + town;
+        textviewheadertown.setText(header);
 
         //stabs
         textViewTodayDay.setText("10");
         textViewTodayNight.setText("5");
         textViewTomorrowDay.setText("12");
         textViewTomorrowNight.setText("6");
-        Log.i(MainActivity.LOGTAG,"  Show Pressure - "+String.valueOf(getIntent().getExtras().getBoolean(MainActivity.PRESSURE)));
-        Log.i(MainActivity.LOGTAG,"  Show Humidity - "+String.valueOf(getIntent().getExtras().getBoolean(MainActivity.HUMIDITY)));
-        Log.i(MainActivity.LOGTAG,"  Show WindSpeed - "+String.valueOf(getIntent().getExtras().getBoolean(MainActivity.WINDSPEED)));
-        Log.i(MainActivity.LOGTAG,"  Show WindDirection - "+String.valueOf(getIntent().getExtras().getBoolean(MainActivity.WINDDIR)));
-        Log.i(MainActivity.LOGTAG,"  Show Precipitation - "+String.valueOf(getIntent().getExtras().getBoolean(MainActivity.PRECIPITATION)));
+        Log.i(MainActivity.LOGTAG, "  Show Pressure - " + String.valueOf(getIntent().getExtras().getBoolean(MainActivity.PRESSURE)));
+        Log.i(MainActivity.LOGTAG, "  Show Humidity - " + String.valueOf(getIntent().getExtras().getBoolean(MainActivity.HUMIDITY)));
+        Log.i(MainActivity.LOGTAG, "  Show WindSpeed - " + String.valueOf(getIntent().getExtras().getBoolean(MainActivity.WINDSPEED)));
+        Log.i(MainActivity.LOGTAG, "  Show WindDirection - " + String.valueOf(getIntent().getExtras().getBoolean(MainActivity.WINDDIR)));
+        Log.i(MainActivity.LOGTAG, "  Show Precipitation - " + String.valueOf(getIntent().getExtras().getBoolean(MainActivity.PRECIPITATION)));
 
 
     }
