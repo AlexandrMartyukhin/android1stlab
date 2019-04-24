90b490a80cd3cbcc8b52e79ebbd0555286e1f803
AlexandrMartyukhin <megacraft@gmail.com>
2019-04-24 16:35:27 +0300

    repaired proj

--- app/src/main/java/ru/minilan/helloworldlab/MainActivity.java
+++ app/src/main/java/ru/minilan/helloworldlab/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(LOGTAG, "MAIN onCreate()...");
     }
 
     public void onClickOkButton(View view) {
